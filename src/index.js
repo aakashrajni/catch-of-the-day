20b3d8de52bed69a0c6d3896ababa418afd2f4c4
aakash <aakash121296@google.com>
2019-05-22 22:49:01 +0530

    Basic design complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
-import './css/style.css';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-render (<StorePicker/>,document.querySelector('#main'));
+import './css/style.css';
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
